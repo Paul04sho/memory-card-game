1f259af4517b4b2f329656dc479a92e32ed5c7a4
Paul- Emmanuel <paultchoume905@gmail.com>
2026-01-10 16:01:37 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const container = document.querySelector(".cards-container");
+const score = document.getElementById("score");
+const restartButton = document.getElementById("restart-btn");
+
+const cardData= [
+    // Eléments du jeu
+]
+
+// Crée une paire pour chaque image chargée
+let cards = [...cardData, ...cardData];
+
+
